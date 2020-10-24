30334837ca9d66a70a93c330b093cad68507e12b
Tim Sheard <tim.sheard@IOHK.io>
2020-10-23 21:56:58 -0700

    Smart Coders added.

--- semantics/executable-spec/src/Data/Coders.hs
+++ semantics/executable-spec/src/Data/Coders.hs
@@ -52,6 +52,7 @@ module Data.Coders
   )
 where
 
+import Data.Array
 import Cardano.Prelude (cborError)
 import Control.Monad (replicateM,unless)
 import Codec.CBOR.Decoding (Decoder)
@@ -232,9 +233,14 @@ data Wrapped = Open | Closed
 data Encode (w :: Wrapped) t where
   Sum :: t -> Word -> Encode 'Open t
   Rec :: t -> Encode 'Closed t
-  To :: ToCBOR a => a -> Encode 'Closed a
+  To :: (FromCBOR a,ToCBOR a) => a -> Encode 'Closed a
   E :: (t -> Encoding) -> t -> Encode 'Closed t
   ApplyE :: Encode w (a -> t) -> Encode 'Closed a -> Encode w t
+  OmitC :: FromCBOR t => t -> Encode 'Closed t
+  Omit:: (t -> Bool) -> Encode Closed t -> Encode Closed t
+  Fold :: Foldable f => f (Item w t) -> Encode w t
+
+data Item (w::Wrapped) t = Item Int (Encode w t)
 
 -- The Wrapped index of ApplyE is determined by the index
 -- at the bottom of its left spine. The LEFT arg of ApplyE
@@ -255,6 +261,8 @@ runE (Rec c) = c
 runE (ApplyE f x) = runE f (runE x)
 runE (To x) = x
 runE (E _ x) = x
+runE (OmitC x) = x
+runE (Omit p x) = runE x
 
 gsize :: Encode w t -> Word
 gsize (Sum _ _) = 0
@@ -262,6 +270,8 @@ gsize (Rec _) = 0
 gsize (To _) = 1
 gsize (E _ _) = 1
 gsize (ApplyE f x) = gsize f + gsize x
+gsize (OmitC x) = 0
+gsize (Omit p x) = if p (runE x) then 0 else gsize x
 
 encode :: Encode w t -> Encoding
 encode sym = encodeCountPrefix 0 sym
@@ -278,6 +288,9 @@ encode sym = encodeCountPrefix 0 sym
     -- by design. Its left spine must end in a (Rec c). We count (gsize x)
     -- the 'fields' in x, and add them to the number things we
     -- must add to the prefix of the enclosing type.
+    encodeCountPrefix _ (OmitC x) = mempty
+    encodeCountPrefix n (Omit p x) =
+       if p (runE x) then mempty else encodeCountPrefix n x
 
     encodeClosed :: Encode 'Closed t -> Encoding
     -- encodeClosed (Sum _ _) -- By design this case is unreachable by type considerations.
@@ -285,6 +298,9 @@ encode sym = encodeCountPrefix 0 sym
     encodeClosed (To x) = toCBOR x
     encodeClosed (E enc x) = enc x
     encodeClosed (ApplyE f x) = encodeClosed f <> encodeClosed x
+    encodeClosed (OmitC n) = mempty
+    encodeClosed (Omit p x) =
+       if p (runE x) then mempty else encodeClosed x
 
 -- =====================
 data Decode (w :: Wrapped) t where
@@ -296,6 +312,18 @@ data Decode (w :: Wrapped) t where
   ApplyD :: Decode w (a -> t) -> Decode 'Closed a -> Decode w t
   Invalid :: Word -> Decode w t
   Map :: (a -> b) -> Decode w a -> Decode w b
+  OmitD :: t -> Decode 'Closed t
+
+instance Show (Decode w t) where
+  show (Summands str f) = "(Summands "++str++")"
+  show (SumD t) = "SumD"
+  show (RecD t) = "RecD"
+  show From = "From"
+  show (D x) = "D"
+  show (ApplyD x y) = "("++show x++" <! "++show y++")"
+  show (Invalid n) = "(Invalid "++show n++")"
+  show (Map f x) = "Map"
+  show (OmitD x) = "OmitD" -- "(OmitD "++show x++")"
 
 infixl 4 <!
 
@@ -311,6 +339,7 @@ hsize (D _) = 1
 hsize (ApplyD f x) = hsize f + hsize x
 hsize (Invalid _) = 0
 hsize (Map _ x) = hsize x
+hsize (OmitD n) = 0
 
 decode :: Decode w t -> Decoder s t
 decode x = fmap snd (decodE x)
@@ -330,6 +359,7 @@ decodeCount (ApplyD c g) n = do
   pure (i, f y)
 decodeCount (Invalid k) _ = invalidKey k
 decodeCount (Map f x) n = do (m, y) <- decodeCount x n; pure (m, f y)
+decodeCount (OmitD x) n = pure(n,x)
 
 decodeClosed :: Decode 'Closed t -> Decoder s t
 decodeClosed (Summands nm f) = decodeRecordSum nm (\x -> decodE (f x))
@@ -343,6 +373,7 @@ decodeClosed (ApplyD c g) = do
   pure (f y)
 decodeClosed (Invalid k) = invalidKey k
 decodeClosed (Map f x) = f <$> decodeClosed x
+decodeClosed (OmitD n) = pure n
 
 instance Functor (Decode w) where
   fmap f (Map g x) = Map (f . g) x
@@ -368,8 +399,122 @@ encodeSet :: ToCBOR a => Set a -> Encoding
 encodeSet = encodeFoldable
 
 -- ===========================================
--- For a worked out EXAMPLE see the testfile:
--- cardano-ledger-specs/shelley/chain-and-ledger/shelley-spec-ledger-test/test/Test/Shelley/Spec/Ledger/MemoBytes.hs
+-- For more worked out EXAMPLES see the testfile:
+-- cardano-ledger-specs/shelley-ma/impl/test/Test/Cardano/Ledger/ShelleyMA/Timelocks.hs
 
-roundTrip :: (ToCBOR t,FromCBOR t) => t -> Either Codec.CBOR.Read.DeserialiseFailure (Lazy.ByteString, t)
+roundTrip :: (ToCBOR t,FromCBOR t) => t ->
+             Either Codec.CBOR.Read.DeserialiseFailure (Lazy.ByteString, t)
 roundTrip s = deserialiseFromBytes fromCBOR (toLazyByteString (toCBOR s))
+
+
+roundTrip' ::(t ->  Encoding) -> (forall s. Decoder s t) -> t ->
+           Either Codec.CBOR.Read.DeserialiseFailure (Lazy.ByteString, t)
+roundTrip' enc dec t = deserialiseFromBytes dec  (toLazyByteString (enc t))
+
+
+-- ==============================
+-- Toys to play with
+-- Not encoding default values using smart Coders
+-- Use Sum, even the type has only one constructor.
+-- The Sum encodes virtual construtors with varying
+-- number of arguments, the missing ones filled with
+-- Omitted values
+
+data A = A Int [Bool]
+  deriving Show
+
+foo :: A -> (Encode 'Open A)
+foo (A 0 []) = (Sum A 0 !> OmitC 0 !> OmitC [])
+foo (A 0 xs) = (Sum A 1 !> OmitC 0 !> To xs)
+foo (A n []) = (Sum A 2 !> To n !> OmitC [])
+foo (A n xs) = (Sum A 3 !> To n !> To xs)
+
+smartEncode :: (t -> Encode 'Open t) -> t -> Encoding
+smartEncode f x = encode (f x)
+
+bar :: Word -> Decode 'Open A
+bar 0 = SumD A <! OmitD 0 <! OmitD []
+bar 1 = SumD A <! OmitD 0 <! From
+bar 2 = SumD A <! From <! OmitD []
+bar 3 = SumD A <! From <! From
+
+smartDecode :: (Word -> Decode 'Open t) -> Decoder s t
+smartDecode f = decodeRecordSum "smart" help
+  where help word = decodE (f word)
+
+instance ToCBOR A where
+  toCBOR = smartEncode foo
+
+instance FromCBOR A where
+  fromCBOR = smartDecode bar
+
+-- ============================================================
+-- We can generate things like foo and bar using 1 liners
+
+foo' :: Word -> A -> (Encode 'Open A)
+foo' tag (A n xs) = (Sum A tag !> Omit (==0) (To n) !> Omit null (To xs))
+
+-- indicate which things can be omitted,
+-- and the value to supply if they are omitted
+
+spec :: Encode 'Open A
+spec = (Sum A 99 !> OmitC 0 !> Omit null (To []))
+
+bar' :: Word -> Decode 'Open A
+bar' = makeDecoder spec
+
+-- ===============================================================
+-- The idea is to make a table driven version of bar, given a tag
+-- index into a table, and return the result.
+-- enumerate the rows of the table in binary counting order
+-- So if we have three Omits then the index will count by
+-- 0 0 0  index = 0,  all three are omitted
+-- 0 0 1  index = 1,  the first two are omitted
+-- 0 1 0  index = 2,  the first and third are omitted
+-- 0 1 1  etc
+
+makeDecoder :: Encode 'Open t -> (Word -> Decode 'Open t)
+makeDecoder spec = \ n -> table ! n  where
+   choices :: Encode w t -> [Decode w t]
+   choices (ApplyE f x) = [ApplyD g y | g <- choices f, y <- choices x]
+   choices (Omit p e) =  (OmitD (runE e)) : choices e
+   choices (OmitC n) = [OmitD n,From]
+   choices (To x) = [From]
+   choices (Sum c n) = [SumD c]
+   choices (Rec c) = [RecD c]
+
+   addTag :: [Decode w t] -> Array Word (Decode w t)
+   addTag xs = array (0,n) (label 0 xs)
+     where n = fromIntegral (length xs) - 1
+           label i (x:xs) = (i,x) : label (i+1) xs
+           label i [] = []
+   table = addTag (choices spec)
+
+
+
+test0 = roundTrip' (smartEncode foo) (smartDecode bar) (A 0 [])
+test1 = roundTrip' (smartEncode foo) (smartDecode bar) (A 0 [True])
+test2 = roundTrip' (smartEncode foo) (smartDecode bar) (A 9 [])
+test3 = roundTrip' (smartEncode foo) (smartDecode bar) (A 9 [True])
+
+test0a = roundTrip' (smartEncode $ foo' 0) (smartDecode bar) (A 0 [])
+test1a = roundTrip' (smartEncode $ foo' 1) (smartDecode bar) (A 0 [True])
+test2a = roundTrip' (smartEncode $ foo' 2) (smartDecode bar) (A 9 [])
+test3a = roundTrip' (smartEncode $ foo' 3) (smartDecode bar) (A 9 [True])
+
+test0b = roundTrip' (smartEncode foo) (smartDecode bar') (A 0 [])
+test1b = roundTrip' (smartEncode foo) (smartDecode bar') (A 0 [True])
+test2b = roundTrip' (smartEncode foo) (smartDecode bar') (A 9 [])
+test3b = roundTrip' (smartEncode foo) (smartDecode bar') (A 9 [True])
+
+test0c = roundTrip' (smartEncode $ foo' 0) (smartDecode bar') (A 0 [])
+test1c = roundTrip' (smartEncode $ foo' 1) (smartDecode bar') (A 0 [True])
+test2c = roundTrip' (smartEncode $ foo' 2) (smartDecode bar') (A 9 [])
+test3c = roundTrip' (smartEncode $ foo' 3) (smartDecode bar') (A 9 [True])
+
+ok = all isRight [ test0,  test1,  test2,  test3
+                 , test0a, test1a, test2a, test3a
+                 , test0b, test1b, test2b, test3b
+                 , test0c, test1c, test2c, test3c ]
+     where isRight (Right x) = True
+           isRight (Left x) = False
